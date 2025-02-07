8b6f40ed2b808a70c59cb3c391598aa78c8efe3e
toadkiller24 <daotoad676@gmail.com>
2025-02-07 16:38:42 +0200

    Removed NFT mint function

--- contracts/ToadLottery1.0.sol
+++ contracts/ToadLottery1.0.sol
@@ -6,11 +6,7 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
-interface IToadNFT {
-    function mint1(address to) payable external;
-}
-
-contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
+contract DecentralizedLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
     event RequestSent(uint256 requestId, uint32 numWords);
     event RequestFulfilled(uint256 requestId, uint256[] randomWords);
     event RequestedRandomness(uint256 requestId);
@@ -18,9 +14,10 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
     event WinnersSelected(address payable[3] winners, uint256[] amounts);
     event WithdrawalMade(address indexed recipient, uint256 amount);
 
-    address constant public TOAD_NFT = 0x1e0ecdc616C548413F67A61B1c448548Ed4E5CDe;
-    address constant public TOAD_ADDRESS = 0x1e0ecdc616C548413F67A61B1c448548Ed4E5CDe; 
-    address constant public SOUP_KITCHEN = 0x1e0ecdc616C548413F67A61B1c448548Ed4E5CDe; 
+    address public constant TOAD_ADDRESS =
+        0x370a366f402e2e41CDBbE54EcEC12aaE0cce1955;
+    address public constant SOUP_KITCHEN =
+        0x000000000000000000000000000000000000dEaD;
 
     struct RequestStatus {
         bool fulfilled;
@@ -32,11 +29,13 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
     uint256 public s_subscriptionId;
     uint256[] public requestIds;
     uint256 public lastRequestId;
-    bytes32 public keyHash = 0x8596b430971ac45bdf6088665b9ad8e8630c9d5049ab54b14dff711bee7c0e26;
+    bytes32 public keyHash =
+        0x8596b430971ac45bdf6088665b9ad8e8630c9d5049ab54b14dff711bee7c0e26;
     uint32 public callbackGasLimit = 2500000;
     uint16 public requestConfirmations = 3;
     uint32 public numWords = 3;
 
+    // State Variables
     address payable[] public players;
     address payable[3] public recentWinners;
     address payable public immutable devAddress;
@@ -44,16 +43,17 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
     uint256[] public latestRandomWords;
     bool public winnersSelected;
     bool public prizesDistributed;
-    
-    uint256 public constant MAXIMUM_PLAYERS = 10;
+
+    // Constants
+    uint256 public constant MAXIMUM_PLAYERS = 4;
     uint256 public constant ENTRY_FEE = 0.001 ether;
-    uint256 public constant MINT_FEE = 0.00001 ether;
     uint256 public constant FINAL_PRIZE_POOL = ENTRY_FEE * MAXIMUM_PLAYERS;
-    
+
+    // Tracking
     mapping(address => uint256) public pendingWithdrawals;
     mapping(address => bool) public isWinner;
     mapping(address => bool) public hasParticipated;
-    
+
     enum LOTTERY_STATE {
         OPEN,
         CLOSED,
@@ -63,6 +63,7 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
 
     IUniswapV2Router02 public uniswapRouter;
 
+    // Custom errors
     error Lottery__InvalidEntry();
     error Lottery__MaxPlayersReached();
     error Lottery__InvalidState();
@@ -77,12 +78,8 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
 
     constructor(
         uint256 subscriptionId,
-        address payable _devAddress,
-        address _uniswapRouter
+        address payable _devAddress
     ) VRFConsumerBaseV2Plus(0xDA3b641D438362C440Ac5458c57e00a712b66700) {
-        require(_devAddress != address(0), "Invalid dev address");
-        require(_uniswapRouter != address(0), "Invalid router address");
-        
         fundingAddresses = [
             payable(0xcD71336769347f8A2B5db79d2606Bef6bf36Ee93),
             payable(0xadC5d469f631333BC3aF98776614aC9bEBD3FAf9)
@@ -90,23 +87,24 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
         s_subscriptionId = subscriptionId;
         lottery_state = LOTTERY_STATE.OPEN;
         devAddress = _devAddress;
-        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+        uniswapRouter = IUniswapV2Router02(
+            0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+        );
     }
 
     function enter() public payable nonReentrant {
         if (msg.value != ENTRY_FEE) revert Lottery__InvalidEntry();
         if (lottery_state != LOTTERY_STATE.OPEN) revert Lottery__InvalidState();
-        if (players.length >= MAXIMUM_PLAYERS) revert Lottery__MaxPlayersReached();
-        
+        if (players.length >= MAXIMUM_PLAYERS)
+            revert Lottery__MaxPlayersReached();
+
         for (uint256 i = 0; i < players.length; i++) {
             if (players[i] == msg.sender) revert Lottery__DuplicateEntry();
         }
-        
-        IToadNFT(TOAD_NFT).mint1{value:MINT_FEE}(msg.sender);
-        
+
         players.push(payable(msg.sender));
         hasParticipated[msg.sender] = true;
-        
+
         emit LotteryEnter(msg.sender);
 
         if (players.length == MAXIMUM_PLAYERS) {
@@ -129,9 +127,7 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
                 callbackGasLimit: callbackGasLimit,
                 numWords: numWords,
                 extraArgs: VRFV2PlusClient._argsToBytes(
-                    VRFV2PlusClient.ExtraArgsV1({
-                        nativePayment: false
-                    })
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
                 )
             })
         );
@@ -151,94 +147,85 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
         uint256[] calldata _randomWords
     ) internal override {
         require(s_requests[_requestId].exists, "request not found");
-        
+
         s_requests[_requestId].fulfilled = true;
         s_requests[_requestId].randomWords = _randomWords;
-        
+
         delete latestRandomWords;
-        for(uint i = 0; i < _randomWords.length; i++) {
+        for (uint i = 0; i < _randomWords.length; i++) {
             latestRandomWords.push(_randomWords[i]);
         }
-        
+
         emit RequestFulfilled(_requestId, _randomWords);
-        
-        _selectWinners();
     }
 
-    function _selectWinners() internal {
-        if (latestRandomWords.length < numWords) revert Lottery__NoRandomWords();
+    function selectWinners() external {
+        if (latestRandomWords.length < numWords)
+            revert Lottery__NoRandomWords();
         if (winnersSelected) revert Lottery__WinnersAlreadySelected();
         if (players.length != MAXIMUM_PLAYERS) revert Lottery__InvalidState();
-        
+
         bool[] memory used = new bool[](MAXIMUM_PLAYERS);
-        
+
         for (uint256 i = 0; i < 3; i++) {
             uint256 index = latestRandomWords[i] % MAXIMUM_PLAYERS;
-            
+
             while (used[index]) {
                 index = (index + 1) % MAXIMUM_PLAYERS;
             }
             used[index] = true;
-            
+
             address payable winner = players[index];
             recentWinners[i] = winner;
             isWinner[winner] = true;
         }
-        
+
         winnersSelected = true;
         emit WinnersSelected(recentWinners, new uint256[](3));
-        
-        _distributePrizes();
+
+        distributePrizes();
     }
-    
-    function _distributePrizes() internal {
+
+    function distributePrizes() internal {
         if (!winnersSelected) revert Lottery__WinnersNotSelected();
         if (prizesDistributed) revert Lottery__PrizesAlreadyDistributed();
-        
+
         uint256 winnersShare = (FINAL_PRIZE_POOL * 60) / 100;
         uint256 singleWinnerShare = winnersShare / 3;
         uint256 devShare = (FINAL_PRIZE_POOL * 5) / 100;
         // uint256 contractRemainder = (FINAL_PRIZE_POOL * 5) / 100;
         uint256 fundingSharePerAddress = (FINAL_PRIZE_POOL * 10) / 100;
-        
+
         for (uint256 i = 0; i < 3; i++) {
             pendingWithdrawals[recentWinners[i]] += singleWinnerShare;
         }
-        
+
         pendingWithdrawals[devAddress] += devShare;
         pendingWithdrawals[fundingAddresses[0]] += fundingSharePerAddress;
         pendingWithdrawals[fundingAddresses[1]] += fundingSharePerAddress;
-        
+
         uint256[] memory amounts = new uint256[](3);
         for (uint256 i = 0; i < 3; i++) {
             amounts[i] = singleWinnerShare;
         }
-        
+
         emit WinnersSelected(recentWinners, amounts);
-        
+
         delete players;
         prizesDistributed = true;
         lottery_state = LOTTERY_STATE.OPEN;
-        
-        _startNewRound();
-    }
-    
-    function _startNewRound() internal {
-        require(prizesDistributed, "Previous round not complete");
-        winnersSelected = false;
-        prizesDistributed = false;
     }
 
     function burnToads() external {
         uint256 weiAmount = address(this).balance;
         require(weiAmount > 0, "Contract has no Ether to swap");
-        
+
         address[] memory path = new address[](2);
         path[0] = uniswapRouter.WETH();
         path[1] = TOAD_ADDRESS;
-        
+
         uint256 deadline = block.timestamp + 180;
-        
+
         uniswapRouter.swapExactETHForTokens{value: weiAmount}(
             0,
             path,
@@ -247,52 +234,46 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
         );
     }
 
-    function selectWinners() external {
-        _selectWinners();
-    }
-    
-    function distributePrizes() external {
-        _distributePrizes();
-    }
-    
-    function startNewRound() external {
-        _startNewRound();
-    }
+    // function startNewRound() external {
+    //     require(prizesDistributed, "Previous round not complete");
+    //     winnersSelected = false;
+    //     prizesDistributed = false;
+    // }
 
     function withdraw() public nonReentrant {
         uint256 amount = pendingWithdrawals[msg.sender];
         if (amount == 0) revert Lottery__NoWithdrawalAvailable();
-        
+
         pendingWithdrawals[msg.sender] = 0;
-        
-        (bool success,) = msg.sender.call{value: amount}("");
+
+        (bool success, ) = msg.sender.call{value: amount}("");
         if (!success) revert Lottery__WithdrawalFailed();
-        
+
         emit WithdrawalMade(msg.sender, amount);
     }
 
-    function getRequestStatus(
-        uint256 _requestId
-    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
-        require(s_requests[_requestId].exists, "request not found");
-        RequestStatus memory request = s_requests[_requestId];
-        return (request.fulfilled, request.randomWords);
-    }
+    // function getRequestStatus(
+    //     uint256 _requestId
+    // ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+    //     require(s_requests[_requestId].exists, "request not found");
+    //     RequestStatus memory request = s_requests[_requestId];
+    //     return (request.fulfilled, request.randomWords);
+    // }
 
-    function getWinningNumbers() external view returns (uint[3] memory) {
-        require(latestRandomWords.length >= numWords, "Random numbers not yet available");
-        
-        uint[3] memory finalNumbers;
-        for(uint i = 0; i < 3 && i < latestRandomWords.length; i++) {
-            finalNumbers[i] = latestRandomWords[i] % 20;
-        }
-        
-        return finalNumbers;
-    }
+    // function getWinningNumbers() external view returns (uint[3] memory) {
+    //     require(latestRandomWords.length >= numWords, "Random numbers not yet available");
 
-    function hasRandomNumbers() external view returns (bool) {
-        return latestRandomWords.length >= numWords;
-    }
+    //     uint[3] memory finalNumbers;
+    //     for(uint i = 0; i < 3 && i < latestRandomWords.length; i++) {
+    //         finalNumbers[i] = latestRandomWords[i] % 20;
+    //     }
+
+    //     return finalNumbers;
+    // }
+
+    // function hasRandomNumbers() external view returns (bool) {
+    //     return latestRandomWords.length >= numWords;
+    // }
 
     function getPlayers() public view returns (address payable[] memory) {
         return players;
@@ -301,10 +282,18 @@ contract toadLottery is VRFConsumerBaseV2Plus, ReentrancyGuard {
     function getPlayerStatus(
         address player
     ) public view returns (bool participated, bool won, uint256 pendingAmount) {
-        return (hasParticipated[player], isWinner[player], pendingWithdrawals[player]);
+        return (
+            hasParticipated[player],
+            isWinner[player],
+            pendingWithdrawals[player]
+        );
     }
 
-    function getRecentWinners() public view returns (address payable[3] memory) {
+    function getRecentWinners()
+        public
+        view
+        returns (address payable[3] memory)
+    {
         return recentWinners;
     }
 }
